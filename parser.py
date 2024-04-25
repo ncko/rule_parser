f4cf2c26a8b92ec13e9d83e7d9871f19b50bae49
ncko <nick.olsen@crossfit.com>
2024-04-25 09:03:47 -0700

    init

---
+++ parser.py
@@ -0,0 +1,193 @@
+"""
+expression  -> logical_or
+logical_or  -> logical_and ("or" logical_or)*
+logical_and -> primary ("and" logical_and)*
+primary     -> RULE | "(" expression ")"
+"""
+
+from pprint import pprint
+from enum import Enum
+
+example1 = "foo and bar or baz";
+example2 = "(foo and bar) or baz";
+example3 = "foo and (bar or baz)";
+
+TokenType = Enum("TokenType", [
+    "OR",
+    "AND",
+    "LEFT_PAREN",
+    "RIGHT_PAREN",
+    "RULE"
+])
+
+tokensMap = {
+    "or": TokenType.OR,
+    "and": TokenType.AND,
+    "(": TokenType.LEFT_PAREN,
+    ")": TokenType.RIGHT_PAREN
+}
+
+class Token:
+    def __init__(self, lexeme, column):
+        self.type = tokensMap.get(lexeme, TokenType.RULE)
+        self.literal = lexeme if self.type == TokenType.RULE else None
+        self.column = column
+
+
+class Scanner:
+    def __init__(self, source):
+        self.source = source
+
+    def scan(self):
+        lexeme = ''
+        tokens = []
+
+        for i, char in enumerate(self.source):
+            isAtEnd = i >= len(self.source) - 1
+
+            if char in { "(", ")" }:
+                if lexeme:
+                    tokens.append(Token(lexeme, i - len(lexeme)))
+                    lexeme = ''
+
+                tokens.append(Token(char, i))
+                continue
+
+            if lexeme and (char.isspace() or isAtEnd):
+
+                if isAtEnd and not char.isspace():
+                    lexeme += char
+                    i += 1
+
+                tokens.append(Token(lexeme, i - len(lexeme)))
+                lexeme = ''
+                continue
+
+            if char.isspace():
+                continue
+
+            lexeme += char
+
+        return tokens
+
+
+
+def printError(token, message):
+    print(message)
+
+
+class ParseError(Exception):
+    pass
+
+
+class Expr:
+    def __init__(self, type):
+        self.type = type
+
+
+class Binary(Expr):
+    def __init__(self, left, operator, right):
+        self.left = left
+        self.operator = operator
+        self.right = right
+        
+
+class Literal(Expr):
+    def __init__(self, literal):
+        self.literal = literal
+
+
+class Grouping(Expr):
+    def __init__(self, expression):
+        self.expression = expression
+
+
+class Parser:
+    def __init__(self, tokens):
+        self.current = 0
+        self.tokens = tokens
+
+    def parse(self):
+        try:
+            return self.expression()
+        except ParseError:
+            return None
+
+    def expression(self):
+        return self.logical_or()
+
+    def logical_or(self):
+        expr = self.logical_and()
+
+        while self.match([TokenType.OR]):
+            operator = self.tokens[self.current - 1]
+            right = self.logical_and()
+            expr = Binary(expr, operator, right)
+
+        return expr
+
+    def logical_and(self):
+        expr = self.primary()
+
+        while self.match([TokenType.AND]):
+            operator = self.tokens[self.current - 1]
+            right = self.primary()
+            expr = Binary(expr, operator, right)
+
+        return expr
+
+    def primary(self):
+        if self.match([TokenType.RULE]):
+            return Literal(self.tokens[self.current - 1])
+
+        if self.match([TokenType.LEFT_PAREN]):
+            expr = self.expression()
+            self.consume(TokenType.RIGHT_PAREN, "Expected ')' after expression.")
+            return Grouping(expr)
+        
+        raise self.error(self.peek(), "Expected an expression")
+
+    def consume(self, type, message):
+        if self.check(type):
+            return self.advance()
+        raise self.error(peek(), message)
+
+    def error(self, token, message):
+        printError(token, message)
+        return ParseError()
+
+    def advance(self):
+        if self.current >= len(self.tokens):
+            return None
+
+        token = self.tokens[self.current]
+        self.current += 1
+        return token
+
+    def match(self, types):
+        for type in types:
+            if self.check(type):
+                self.advance()
+                return True
+
+        return False
+
+    def check(self, type):
+        if self.isAtEnd():
+            return False
+
+        return self.peek().type == type
+
+
+    def peek(self):
+        return self.tokens[self.current]
+    
+    def isAtEnd(self):
+        return self.current >= len(self.tokens)
+
+
+if __name__ == "__main__":
+    scanner = Scanner(example2)
+    tokens = scanner.scan()
+    parser = Parser(tokens)
+    expression = parser.parse()
